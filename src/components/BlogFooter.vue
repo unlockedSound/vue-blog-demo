41150e7a500b177bcbb28d036cea62a558e05275
Chorvinsky <david.chorvinsky@dc.gov>
2017-11-20 15:36:14 -0500

    pushing random stuff still on work comp

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -2,7 +2,7 @@
   <transition appear name="v--mask">
     <footer v-once class="blog__footer">
       <a class="signature" href="https://snipcart.com" target="_blank">
-        <img src="../assets/vue-snip.svg"/>
+
       </a>
     </footer>
   </transition>
